f270c4338a609d0120c6e8ff514e879a1c37b5be
Julian Gindi <_@gindi.io>
2016-11-14 17:07:25 -0800

    working prototype...will clean up eventually

--- diceware.py
+++ diceware.py
@@ -1,33 +1,42 @@
+import sys
 import random
 import time
 
+# Seeding RNG with current time for now
 random.seed(time.time())
-
 
 # roll_die returns a random number used to look up words in the wordlist.
 # We 'virtually' roll a dice 5 times to generate the number to use.
 def roll_dice():
     final_number = ''
-
     for roll in range(5):
-        roll = randint(1, 6)
+        roll = random.randint(1, 6)
         final_number += str(roll)
-
-    return final_number
+    return int(final_number)
 
 
 # lookup_associated_word takes a combination of dice rolls and
 # returns the associated word
 def lookup_associated_word(dice_number):
-    # 1. Read file
     with open("eff_large_wordlist.txt") as wordlist:
-        pass
-    # 2. Lookup word associated with dice_number
-    # 3. Return word
-    pass
+        for line in wordlist:
+            num, word = line.split('\t')
+            if int(num) == dice_number:
+                return word.strip()
 
 
 # generate_diceware_password: Takes in a length representing
 # the number of words that will compose the final passphrase
 def generate_diceware_password(passphrase_length):
     passphrase_list = []
+
+    for iter in range(passphrase_length):
+        roll = roll_dice()
+        passphrase_list.append(lookup_associated_word(roll))
+
+    return ' '.join(passphrase_list)
+
+
+if __name__ == '__main__':
+    rolls = int(sys.argv[1])
+    print(generate_diceware_password(rolls))
